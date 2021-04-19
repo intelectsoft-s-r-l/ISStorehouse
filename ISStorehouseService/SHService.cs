b4aae0cd0692119e09c58df50b8151ff9ba76871
BreahnaLilian <49956224+BreahnaLilian@users.noreply.github.com>
2021-04-19 18:05:08 +0300

    service created

--- ISStorehouseService/SHService.cs
+++ ISStorehouseService/SHService.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.ComponentModel;
 using System.Configuration;
+using System.Configuration.Install;
 using System.Diagnostics;
 using System.ServiceModel;
 using System.ServiceModel.Description;
@@ -10,113 +12,131 @@ namespace ISStorehouseService
 {
     public partial class SHService : ServiceBase
     {
+        public ServiceHost serviceHost = null;
         public SHService()
         {
-            InitializeComponent();
-            stopping = false;
-            stoppedEvent = new ManualResetEvent(false);
+            ServiceName = "ISStorehouseService";
+            //InitializeComponent();
+            //stopping = false;
+            //stoppedEvent = new ManualResetEvent(false);
         }
+
+
 
         public void StartService()
         {
-            EventLog.WriteEntry("IS Storehouse service OnStart");
-            ThreadPool.QueueUserWorkItem(new WaitCallback(SHServiceWorker));
+            //EventLog.WriteEntry("IS Storehouse service OnStart");
+            //ThreadPool.QueueUserWorkItem(new WaitCallback(SHServiceWorker));
         }
 
         protected override void OnStart(string[] args)
         {
-            StartService();
+            if(serviceHost != null)
+            {
+                serviceHost.Close();
+            }
+
+            serviceHost = new ServiceHost(typeof(StorehouseService));
+            serviceHost.Open();
+            //StartService();
         }
 
         protected override void OnStop()
         {
-            EventLog.WriteEntry("IS Storehouse service OnStop");
-            stopping = true;
-            stoppedEvent.WaitOne();
-        }
-
-        private bool stopping;
-        private ManualResetEvent stoppedEvent;
-        private static AppSettingsReader read = new AppSettingsReader();
-        private static bool UpdatedDatabase = false;
-        private ServiceHost ISStorehouseHost;
-
-        internal static string MetaUri
-        {
-            get
+            if (serviceHost != null)
             {
-                return Convert.ToString(read.GetValue("MetaUri", typeof(string)));
+                serviceHost.Close();
+                serviceHost = null;
             }
+
+            //EventLog.WriteEntry("IS Storehouse service OnStop");
+            //stopping = true;
+            //stoppedEvent.WaitOne();
         }
 
-        internal static string Port
-        {
-            get
-            {
-                return Convert.ToString(read.GetValue("Port", typeof(string)));
-            }
-        }
+        //private bool stopping;
+        //private ManualResetEvent stoppedEvent;
+        //private static AppSettingsReader read = new AppSettingsReader();
+        //private static bool UpdatedDatabase = false;
+        //private ServiceHost ISStorehouseHost;
 
-        private void SHServiceWorker(object state)
-        {
-            SHServiceStart();
-            while (!stopping)
-                Thread.Sleep(5000);
-            stoppedEvent.Set();
-        }
+        //internal static string MetaUri
+        //{
+        //    get
+        //    {
+        //        return Convert.ToString(read.GetValue("MetaUri", typeof(string)));
+        //    }
+        //}
 
-        private void SHServiceStart()
-        {
-            try
-            {
-                StorehouseService storehouse = new StorehouseService();
+        //internal static string Port
+        //{
+        //    get
+        //    {
+        //        return Convert.ToString(read.GetValue("Port", typeof(string)));
+        //    }
+        //}
 
-                string ipHost = MetaUri;
-                if (MetaUri == null || ipHost.Length == 0)
-                    ipHost = "localhost";
+        //private void SHServiceWorker(object state)
+        //{
+        //    SHServiceStart();
+        //    while (!stopping)
+        //        Thread.Sleep(5000);
+        //    stoppedEvent.Set();
+        //}
 
-                if (Port != "0")
-                {
-                    string servicename = "ISStorehouseService";
-                    string uriS = "http://{0}:{1}/" + servicename;
+        //private void SHServiceStart()
+        //{
+        //    try
+        //    {
+        //        StorehouseService storehouse = new StorehouseService();
 
-                    uriS = string.Format(uriS, ipHost, Port);
-                    ISStorehouseHost = new ServiceHost(storehouse, new Uri(uriS));
+        //        string ipHost = MetaUri;
+        //        if (MetaUri == null || ipHost.Length == 0)
+        //            ipHost = "localhost";
 
-                    WebHttpBinding httpExportBinding = new WebHttpBinding();
+        //        if (Port != "0")
+        //        {
+        //            string servicename = "ISStorehouseService";
+        //            string uriS = "http://{0}:{1}/" + servicename;
 
-                    httpExportBinding.Security.Transport.ClientCredentialType = HttpClientCredentialType.Basic;
-                    httpExportBinding.Security.Transport.Realm = "";
-                    WebHttpBehavior beh = new WebHttpBehavior();
-                    beh.DefaultOutgoingResponseFormat = System.ServiceModel.Web.WebMessageFormat.Xml;
-                    beh.DefaultBodyStyle = System.ServiceModel.Web.WebMessageBodyStyle.Wrapped;
-                    beh.HelpEnabled = true;
+        //            uriS = string.Format(uriS, ipHost, Port);
+        //            ISStorehouseHost = new ServiceHost(storehouse, new Uri(uriS));
 
-                    var withBlock = ISStorehouseHost.AddServiceEndpoint(typeof(IStorehouseService), httpExportBinding, "xml");
-                    withBlock.Behaviors.Add(beh);
+        //            WebHttpBinding httpExportBinding = new WebHttpBinding();
 
-                    ServiceMetadataBehavior metaBehavior = new ServiceMetadataBehavior();
+        //            httpExportBinding.Security.Transport.ClientCredentialType = HttpClientCredentialType.Basic;
+        //            httpExportBinding.Security.Transport.Realm = "";
+        //            WebHttpBehavior beh = new WebHttpBehavior();
+        //            beh.DefaultOutgoingResponseFormat = System.ServiceModel.Web.WebMessageFormat.Xml;
+        //            beh.DefaultBodyStyle = System.ServiceModel.Web.WebMessageBodyStyle.Wrapped;
+        //            beh.HelpEnabled = true;
 
-                    ISStorehouseHost.Description.Behaviors.Add(metaBehavior);
-                    WSHttpBinding metaBinging = new WSHttpBinding();
+        //            var withBlock = ISStorehouseHost.AddServiceEndpoint(typeof(IStorehouseService), httpExportBinding, "xml");
+        //            withBlock.Behaviors.Add(beh);
 
-                    metaBinging.Security.Transport.ClientCredentialType = HttpClientCredentialType.Basic;
-                    metaBinging.Security.Transport.Realm = "";
-                    ISStorehouseHost.AddServiceEndpoint(typeof(IMetadataExchange), metaBinging, "mex");
+        //            ServiceMetadataBehavior metaBehavior = new ServiceMetadataBehavior();
 
-                    BasicHttpBinding basicHttpBnd = new BasicHttpBinding();
+        //            ISStorehouseHost.Description.Behaviors.Add(metaBehavior);
+        //            WSHttpBinding metaBinging = new WSHttpBinding();
 
-                    basicHttpBnd.Security.Transport.ClientCredentialType = HttpClientCredentialType.Basic;
-                    basicHttpBnd.Security.Transport.Realm = "";
+        //            metaBinging.Security.Transport.ClientCredentialType = HttpClientCredentialType.Basic;
+        //            metaBinging.Security.Transport.Realm = "";
+        //            ISStorehouseHost.AddServiceEndpoint(typeof(IMetadataExchange), metaBinging, "mex");
 
-                    var withBlock2 = ISStorehouseHost.AddServiceEndpoint(typeof(IStorehouseService), basicHttpBnd, "soap");
+        //            BasicHttpBinding basicHttpBnd = new BasicHttpBinding();
 
-                }
-            }
-            catch (Exception ex)
-            {
-                EventLog.WriteEntry(ex.ToString());
-            }
-        }
+        //            basicHttpBnd.Security.Transport.ClientCredentialType = HttpClientCredentialType.Basic;
+        //            basicHttpBnd.Security.Transport.Realm = "";
+
+        //            var withBlock2 = ISStorehouseHost.AddServiceEndpoint(typeof(IStorehouseService), basicHttpBnd, "soap");
+
+        //        }
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        EventLog.WriteEntry(ex.ToString());
+        //    }
+        //}
+
     }
 }
